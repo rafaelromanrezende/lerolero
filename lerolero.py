e7070cd8eb25fd4ae99072b003c49e1729f2a465
Rafael <rafaelromanrezende123@usp.br>
2025-03-20 10:53:51 -0300

    nao li 2

--- lerolero.py
+++ lerolero.py
@@ -5,8 +5,20 @@ Gera frases de efeito sem significado real."""
 
 import random
 
-parte1 = []
-parte2 = []
-parte3 = []
+parte1 = [
+"O sistema em desenvolvimento",
+"O novo protocolo de comunicação",
+"O aloritmo otimizado"
+]
+parte2 = [
+"possui excelente desempenho",
+"oferece garantias de precisão acima da média",
+"preenche uma lacuna significativa"
+]
+parte3 = [
+"nas aplicações a que se destina",
+"em relação às opções disponíves no mercado",
+", provendo ampla vantagem vantagem competitiva a seus usuários"
+]
 
 print(random.choice(parte1) ,random.choice(parte2) , random.choice(parte3)) 
