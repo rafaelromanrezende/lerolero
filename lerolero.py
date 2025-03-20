815a6278240f0ead171fd08bc8ae97035e5c08e2
Vinicius Tahara <vini.tahara@usp.br>
2025-03-20 11:11:34 -0300

    AAAAA

--- lerolero.py
+++ lerolero.py
@@ -6,18 +6,18 @@ Gera frases de efeito sem significado real."""
 import random
 
 parte1 = [
-"O sistema em desenvolvimento",
-"O novo protocolo de comunicação",
-"O aloritmo otimizado"
-]
+	"O sistema em desenvolvimento",
+	"O novo protocolo de comunicação",
+	"O aloritmo otimizado"
+	]
 parte2 = [
-"possui excelente desempenho",
-"oferece garantias de precisão acima da média",
-"preenche uma lacuna significativa"
+	"possui excelente desempenho",
+	"oferece garantias de precisão acima da média",
+	"preenche uma lacuna significativa"
 ]
 parte3 = [
-"nas aplicações a que se destina",
-"em relação às opções disponíves no mercado",
+	"nas aplicações a que se destina",
+	"em relação às opções disponíves no mercado",
 ", provendo ampla vantagem vantagem competitiva a seus usuários"
 ]
 
