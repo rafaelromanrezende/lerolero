15bddf332940212a06e78bc06c81913d70b8a409
Rafael <rafaelromanrezende123@usp.br>
2025-03-20 10:33:32 -0300

    Implementa esqueleto do programa

--- lerolero.py
+++ lerolero.py
@@ -2,3 +2,11 @@
 """Gerador de lero-lero.
 
 Gera frases de efeito sem significado real."""
+
+import random
+
+parte1 = []
+parte2 = []
+parte3 = []
+
+print(random.choice(parte1) ,random.choice(parte2) , random.choice(parte3)) 
